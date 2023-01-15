f2ceffd491b563f048e6418d51c07bfe5d03ae65
Tobi DEGNON <tobidegnon@proton.me>
2023-01-15 13:09:35 +0100

    feat: Add cattrs converter for UUID

--- py_listmonk/converter.py
+++ py_listmonk/converter.py
@@ -1,9 +1,17 @@
+from uuid import UUID
+
 from cattrs import Converter
 from pendulum import DateTime
 from pendulum import parse as pendulum_parse
 
 converter = Converter()
+
+# pendulum datetime
 converter.register_unstructure_hook(DateTime, lambda dt: dt.to_iso8601_string())
 converter.register_structure_hook(
     DateTime, lambda isostring, _: pendulum_parse(isostring)
 )
+
+# uuid
+converter.register_unstructure_hook(UUID, lambda uuid: str(uuid))
+converter.register_structure_hook(UUID, lambda uuidstring, _: UUID(uuidstring))
